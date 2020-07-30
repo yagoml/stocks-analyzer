0e83cf4db48b2f9e5bc19f83edd671850f45fb12
yagodml <yagoskor@gmail.com>
2020-07-30 19:16:03 -0300

    sass. typescript update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import GoodCheap from './views/GoodCheap'
+import './App.scss'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/good-indicators"></Route>
+          <Route path="/good-cheap">
+            <GoodCheap />
+          </Route>
+          <Route path="/top-dividend"></Route>
+          <Route path="/cheap"></Route>
+          <Route path="/ibov"></Route>
+          <Route path="/sector-ranking"></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
