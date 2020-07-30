421ba01c138838a62b0c99af2adf38b9e051deba
yagodml <yagoskor@gmail.com>
2020-07-30 20:02:56 -0300

    graphql apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/views/GoodCheap/index.tsx
+++ src/views/GoodCheap/index.tsx
@@ -1,41 +1,99 @@
 import React from 'react'
 import { Table, Container } from 'react-bootstrap'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+interface Price {
+  actualValue: Number
+  minValue: Number
+  maxValue: Number
+  dividend: Number
+}
+
+interface Stock {
+  code: Number
+  price: Price
+  pl: Number
+  pVp: Number
+  roe: Number
+  roa: Number
+  lpa: Number
+  roic: Number
+  netMargin: Number
+  dlPl: Number
+  evEbit: Number
+  cagrRevenue: Number
+  currentLiquidity: Number
+  cagrProfits: Number
+  dlEbit: Number
+  dailyLiquidity: Number
+}
+
+interface GraphqlResponse {
+  data: any
+  loading: Boolean
+}
 
 export default function GoodCheap() {
   return (
     <div className="goodCheap">
-      <Container>
+      <Container fluid>
         <Table bordered hover size="sm">
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Papel</th>
+              <th>Preço</th>
+              <th>ROE</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry the Bird</td>
-              <td>@twitter</td>
-              <td>@twitter</td>
-            </tr>
+            <Query query={GET_GOOD_CHEAP}>
+              {({ data, loading }: GraphqlResponse) => {
+                if (loading) return 'Loading...'
+                return (
+                  data &&
+                  data.goodAndCheap &&
+                  data.goodAndCheap.map((stock: Stock) => (
+                    <tr>
+                      <td>{stock.code}</td>
+                      <td>{stock.price.actualValue.toLocaleString()}</td>
+                      <td>{stock.roe.toLocaleString()} %</td>
+                    </tr>
+                  ))
+                )
+              }}
+            </Query>
           </tbody>
         </Table>
       </Container>
     </div>
   )
 }
+
+const GET_GOOD_CHEAP = gql`
+  {
+    goodAndCheap {
+      code
+      price {
+        actualValue
+        minValue
+        maxValue
+        dividend
+      }
+      pl
+      pVp
+      roe
+      roa
+      lpa
+      roic
+      netMargin
+      dlPl
+      evEbit
+      cagrRevenue
+      currentLiquidity
+      cagrProfits
+      dlEbit
+      dailyLiquidity
+    }
+  }
+`
